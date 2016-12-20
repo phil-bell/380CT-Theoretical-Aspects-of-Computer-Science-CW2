136f41bc279aa3704d4d1753790efc64ad01d6f5
Philip Bell <philhabell@gmail.com>
2016-12-20 21:02:04 +0000

    added part that outputs results

--- dynamicPorgramming.py
+++ dynamicPorgramming.py
@@ -12,3 +12,12 @@ class SSP():
                 table[i][j] = table[i-1][j]
                 if table[i][j]==False and j>=list1[i-1]:
                     table[i][j] = table[i][j] or table[i-1][j-list1[i-1]]
+        return table[len(list1)][tot]
+
+listToSend = [1,2,3,4,5,6,7,8]
+totalToSend = 12
+instance = SSP()
+out = instance.subsetSum(listToSend,totalToSend)
+print ("Set: ", listToSend) #prints out set
+print ("Total: ", totalToSend) #prints total
+print ("Results: ",out) #prints out the values that sum to the total
